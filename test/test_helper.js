8a6f99685af3e2ec50bb1fcea5d3d2f68df5c1c6
Veronica Moreno <frau.veronika@gmail.com>
2016-08-04 12:24:48 -0700

    core voting functions and unit tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,6 @@
-import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
+const chai = require('chai');
+const chaiImmutable = require('chai-immutable');
 
 chai.use(chaiImmutable);
+
+module.exports = chai;
